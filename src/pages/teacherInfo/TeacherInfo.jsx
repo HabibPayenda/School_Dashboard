f0df161b2e15765d71bb6d47e3673f69f4078ac7
Habib <habiburahmanpayenda@gmail.com>
2023-06-17 10:12:50 +0430

    Gets teachers data and renders it in teacher info page

--- src/pages/teacherInfo/TeacherInfo.jsx
+++ src/pages/teacherInfo/TeacherInfo.jsx
@@ -1,12 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./teacherInfo.module.css";
 import TeachersCard from "../../components/teachersCard/TeachersCard";
 import { Link } from "react-router-dom";
 import Modal from "../../components/Modal";
 import AddTeacherModal from "../../Modals/AddTeacherModal";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllTeachers } from "../../store/teachersSlice";
+import TeacherCard from "../../components/dashbordCard/DashBordCard";
 
 function TeacherInfo() {
   const [showModal, setShowModal] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllTeachers());
+  }, []);
+
+  const teachers = useSelector((state) => state.teachers.teachers);
+
+  const renderTeachers = () => {
+    let cards = teachers?.map((teacher) => {
+      return <TeacherCard teacher={teacher} />;
+    });
+    if (teachers?.length > 0) {
+      return cards;
+    } else
+      return <h1 style={{ alignSelf: "center" }}>No teachers added yet</h1>;
+  };
 
   return (
     <div className={styles.teacherInfo}>
@@ -21,13 +41,7 @@ function TeacherInfo() {
           Add Teachers
         </button>
       </div>
-      <div className={styles.cards}>
-        <TeachersCard />
-        <TeachersCard />
-        <TeachersCard />
-        <TeachersCard />
-        <TeachersCard />
-      </div>
+      <div className={styles.cards}>{renderTeachers()}</div>
     </div>
   );
 }
