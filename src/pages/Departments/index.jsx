de6cf6773e4245626e42656e197f404a665e8bb5
Habib <habiburahmanpayenda@gmail.com>
2023-06-19 10:25:58 +0430

    Adds styles to departments page

--- src/pages/Departments/index.jsx
+++ src/pages/Departments/index.jsx
@@ -31,9 +31,7 @@ function Departments() {
         <AddDepartmentModal setShowModal={setShowModal} />
       </Modal>
       <div className={styles.header}>
-        <Link to="/students" className={styles.btn}>
-          Back
-        </Link>
+        <h2 className={styles.title}>Manage Your Departments</h2>
         <button onClick={() => setShowModal(true)} className={styles.btn}>
           Add Department
         </button>
