9df9c9dcc094e91a890a0b25ac9f2f46e30fe013
Habib <habiburahmanpayenda@gmail.com>
2023-06-19 17:23:22 +0430

    Adds styles and icons to attendanceCard

--- src/components/AttendanceCard/index.jsx
+++ src/components/AttendanceCard/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./attendanceCard.module.css";
 import { useDispatch } from "react-redux";
 import { attendanceAction } from "../../store/classesSlice";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 function AttendanceCard({ record }) {
   const { student, status, attendence_id } = record;
@@ -10,22 +12,33 @@ function AttendanceCard({ record }) {
   const hanldeAttendance = (status) => {
     dispatch(attendanceAction({ status: status, id: record.id }));
   };
+
+  let statusText = "";
+  if (status === "pending") {
+    statusText = "Pending";
+  } else if (status === "present") {
+    statusText = <FontAwesomeIcon icon={faCheck} />;
+  } else if (status === "absent") {
+    statusText = <FontAwesomeIcon icon={faXmark} />;
+  }
   return (
     <div className={styles.container}>
-      <p>{student?.name}</p>
-      <p>{status}</p>
+      <p className={styles.name}>{student?.name}</p>
+      <p>{statusText}</p>
       <div className={styles.btnsContainer}>
         <button
           className={styles.btn}
           onClick={() => hanldeAttendance("present")}
         >
           Present
+          <FontAwesomeIcon icon={faCheck} />
         </button>
         <button
           className={styles.btn}
           onClick={() => hanldeAttendance("absent")}
         >
           Absent
+          <FontAwesomeIcon icon={faXmark} />
         </button>
       </div>
     </div>
