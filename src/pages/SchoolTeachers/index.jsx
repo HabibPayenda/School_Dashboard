27140a7a17e6315c272d49eb35e9bc3fcc241513
Habib <habiburahmanpayenda@gmail.com>
2023-06-20 10:55:34 +0430

    Fixes things in SchoolTeachers page

--- src/pages/SchoolTeachers/index.jsx
+++ src/pages/SchoolTeachers/index.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import styles from "./schoolTeachers.module.css";
 
+import teachersImage from "../../assets/teachers.jpg";
+import teacherImage from "../../assets/teacher.jpg";
+
 function SchoolTeachers() {
   return (
     <div>
       {/* Hero section */}
-      <section className="hero">
-        <img
-          src="https://example.com/teachers-image.jpg"
-          alt="Afghan School Teachers"
-        />
+      <section className={styles.hero}>
+        <img src={teachersImage} alt="Afghan School Teachers" />
         <div className={styles["hero-text"]}>
           <h1>Meet Our Afghan School Teachers</h1>
           <p>
@@ -18,7 +18,7 @@ function SchoolTeachers() {
             education that prepares them for success in college and beyond.
             Learn more about our teachers below.
           </p>
-          <button className={styles["login-btn"]}>Login</button>
+          <button className={styles["login-btn"]}>Teacher Login</button>
         </div>
       </section>
 
@@ -27,18 +27,7 @@ function SchoolTeachers() {
         <h2>Our Teachers</h2>
         <div className={styles["teacher-cards"]}>
           <div className={styles["teacher-card"]}>
-            <img src="https://example.com/teacher1.jpg" alt="Teacher 1" />
-            <h3>Ms. Fatima</h3>
-            <p className={styles["teacher-subject"]}>Mathematics</p>
-            <p className={styles["teacher-bio"]}>
-              Ms. Fatima has been teaching mathematics at the Afghan School for
-              over 10 years. She is passionate about helping her students
-              understand complex mathematical concepts and develop critical
-              thinking skills.
-            </p>
-          </div>
-          <div className={styles["teacher-card"]}>
-            <img src="https://example.com/teacher2.jpg" alt="Teacher 2" />
+            <img src={teacherImage} alt="Teacher 2" />
             <h3>Mr. Rahim</h3>
             <p className={styles["teacher-subject"]}>Science</p>
             <p className={styles["teacher-bio"]}>
@@ -49,14 +38,25 @@ function SchoolTeachers() {
             </p>
           </div>
           <div className={styles["teacher-card"]}>
-            <img src="https://example.com/teacher3.jpg" alt="Teacher 3" />
-            <h3>Ms. Zainab</h3>
-            <p className={styles["teacher-subject"]}>English</p>
+            <img src={teacherImage} alt="Teacher 2" />
+            <h3>Mr. Rahim</h3>
+            <p className={styles["teacher-subject"]}>Science</p>
             <p className={styles["teacher-bio"]}>
-              Ms. Zainab is a graduate of Kabul University and has been teaching
-              English at the Afghan School for 5 years. She is dedicated to
-              helping her students improve their reading, writing, and speaking
-              skills.
+              Mr. Rahim is a former engineer who decided to become a teacher to
+              inspire the next generation of Afghan scientists. He believes that
+              hands-on experiments and real-world examples are the key to
+              sparking students' interest in science.
+            </p>
+          </div>
+          <div className={styles["teacher-card"]}>
+            <img src={teacherImage} alt="Teacher 2" />
+            <h3>Mr. Rahim</h3>
+            <p className={styles["teacher-subject"]}>Science</p>
+            <p className={styles["teacher-bio"]}>
+              Mr. Rahim is a former engineer who decided to become a teacher to
+              inspire the next generation of Afghan scientists. He believes that
+              hands-on experiments and real-world examples are the key to
+              sparking students' interest in science.
             </p>
           </div>
         </div>
