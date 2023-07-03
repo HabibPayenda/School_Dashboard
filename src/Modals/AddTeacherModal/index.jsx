2bb9aa43cdc5ee84682b80c1de62bc37b895fb45
Habib <habiburahmanpayenda@gmail.com>
2023-07-03 15:13:24 +0430

    Adds validation to add teacher

--- src/Modals/AddTeacherModal/index.jsx
+++ src/Modals/AddTeacherModal/index.jsx
@@ -3,6 +3,8 @@ import styles from "./addTeacher.module.css";
 import { useDispatch } from "react-redux";
 import { addTeacher } from "../../store/teachersSlice";
 import { useNavigate, useNavigation } from "react-router-dom";
+import validationSchema from "./addTeacherSchema";
+import { Formik } from "formik";
 
 function AddTeacherModal({ setShowModal }) {
   const [name, setName] = useState("");
@@ -34,6 +36,20 @@ function AddTeacherModal({ setShowModal }) {
     setShowModal(false);
   };
 
+  const initialValues = {
+    name: "",
+    email: "",
+    password: "",
+    phone: "",
+    subject: "",
+  };
+
+  const onSubmit = (values) => {
+    console.log(values);
+    dispatch(addTeacher(values));
+    setShowModal(false);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -41,51 +57,97 @@ function AddTeacherModal({ setShowModal }) {
       </div>
       <div className={styles.body}>
         <div className={styles.inputsContainer}>
-          <input
-            className={styles.input}
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            type="text"
-            placeholder="Teacher Name"
-          />
-          <input
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className={styles.input}
-            type="email"
-            placeholder="Email"
-            autocomplete="off"
-          />
-          <input
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className={styles.input}
-            type="password"
-            placeholder="Password"
-          />
-          <input
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-            className={styles.input}
-            type="text"
-            placeholder="Mobile number"
-          />
-          <input
-            value={subject}
-            onChange={(e) => setSubject(e.target.value)}
-            className={styles.input}
-            type="text"
-            placeholder="Subject"
-          />
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+          >
+            {({
+              values,
+              handleChange,
+              handleSubmit,
+              errors,
+              handleBlur,
+              touched,
+            }) => (
+              <form
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "10px",
+                  width: "100%",
+                }}
+                onSubmit={handleSubmit}
+              >
+                <input
+                  className={styles.input}
+                  value={values.name}
+                  onChange={(e) => handleChange(e)}
+                  type="text"
+                  placeholder="Teacher Name"
+                  onBlur={handleBlur}
+                  name="name"
+                />
+                {errors.name && touched.name && <div>{errors.name}</div>}
+                <input
+                  className={styles.input}
+                  value={values.email}
+                  onChange={(e) => handleChange(e)}
+                  type="text"
+                  placeholder="Email"
+                  onBlur={handleBlur}
+                  name="email"
+                />
+                {errors.email && touched.email && <div>{errors.email}</div>}
+                <input
+                  className={styles.input}
+                  value={values.phone}
+                  onChange={(e) => handleChange(e)}
+                  type="text"
+                  placeholder="Mobile number"
+                  onBlur={handleBlur}
+                  name="phone"
+                />
+                {errors.phone && touched.phone && <div>{errors.phone}</div>}
+                <input
+                  className={styles.input}
+                  value={values.password}
+                  onChange={(e) => handleChange(e)}
+                  type="text"
+                  placeholder="Password"
+                  onBlur={handleBlur}
+                  name="password"
+                />
+                {errors.password && touched.password && (
+                  <div>{errors.password}</div>
+                )}
+                <input
+                  className={styles.input}
+                  value={values.subject}
+                  onChange={(e) => handleChange(e)}
+                  type="text"
+                  placeholder="Subject"
+                  onBlur={handleBlur}
+                  name="subject"
+                />
+                {errors.subject && touched.subject && (
+                  <div>{errors.subject}</div>
+                )}
+                <div className={styles.footer}>
+                  <button
+                    onClick={() => setShowModal(false)}
+                    className={styles.btn}
+                  >
+                    Close
+                  </button>
+                  <button onClick={handleSubmit} className={styles.btn}>
+                    Add
+                  </button>
+                </div>
+              </form>
+            )}
+          </Formik>
         </div>
-      </div>
-      <div className={styles.footer}>
-        <button onClick={() => setShowModal(false)} className={styles.btn}>
-          Close
-        </button>
-        <button onClick={handleAddTeacher} className={styles.btn}>
-          Add
-        </button>
       </div>
     </div>
   );
