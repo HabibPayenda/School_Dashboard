5dff534f2c3f73634624392e41677905cb3e2ce6
Habib <habiburahmanpayenda@gmail.com>
2023-07-03 13:54:44 +0430

    Adds yup validation and formik to edit department modal

--- src/Modals/EditDepartmentModal/index.jsx
+++ src/Modals/EditDepartmentModal/index.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import styles from "./editDepartment.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { addDepartment, updateDepartment } from "../../store/departmentsSlice";
-
+import { Formik } from "formik";
+import validationSchema from "./editDepartmentSchema";
 function EditDepartmentModal({ setShowModal }) {
   const department = useSelector((state) => state.departments.showDepartment);
   const [name, setName] = useState(department?.name);
@@ -19,6 +20,17 @@ function EditDepartmentModal({ setShowModal }) {
     setShowModal(false);
   };
 
+  const initialValues = {
+    name: department.name,
+    id: department.id,
+  };
+
+  const onSubmit = (values) => {
+    console.log(values);
+    dispatch(updateDepartment(values));
+    setShowModal(false);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -26,22 +38,45 @@ function EditDepartmentModal({ setShowModal }) {
       </div>
       <div className={styles.body}>
         <div className={styles.inputsContainer}>
-          <input
-            className={styles.input}
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            type="text"
-            placeholder="Department Name"
-          />
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+          >
+            {({
+              values,
+              handleChange,
+              handleSubmit,
+              errors,
+              handleBlur,
+              touched,
+            }) => (
+              <form onSubmit={handleSubmit}>
+                <input
+                  className={styles.input}
+                  value={values.name}
+                  onChange={(e) => handleChange(e)}
+                  type="text"
+                  placeholder="Department Name"
+                  onBlur={handleBlur}
+                  name="name"
+                />
+                {errors.name && touched.name && <div>{errors.name}</div>}
+                <div className={styles.footer}>
+                  <button
+                    onClick={() => setShowModal(false)}
+                    className={styles.btn}
+                  >
+                    Close
+                  </button>
+                  <button onClick={handleSubmit} className={styles.btn}>
+                    Update
+                  </button>
+                </div>
+              </form>
+            )}
+          </Formik>
         </div>
-      </div>
-      <div className={styles.footer}>
-        <button onClick={() => setShowModal(false)} className={styles.btn}>
-          Close
-        </button>
-        <button onClick={handleUpdateDepartment} className={styles.btn}>
-          Update
-        </button>
       </div>
     </div>
   );
